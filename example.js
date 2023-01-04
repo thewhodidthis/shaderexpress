e17885228174a686f259fe17e93a8fe8321ef7c8
thewhodidthis <thewhodidthis@fastmail.com>
2023-01-04 21:34:06 +0000

    git: rework into a single script and example page, drop import map

--- example.js
+++
@@ -1,9 +0,0 @@
-import { ShaderExpress } from "./main.js"
-
-const s = document.querySelector("s-express")
-
-s?.addEventListener("error", function onerror(e) {
-  console.log("error", e?.message)
-})
-
-self.customElements?.define("s-express", ShaderExpress)

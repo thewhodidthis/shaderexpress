2f5dd2bff663fe5389e3b394cc7e1c2c832001de
thewhodidthis <thewhodidthis@fastmail.com>
2023-01-08 00:05:11 +0000

    main: fix event handling, use a textarea, clean up

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ export class ShaderExpress extends Dummy {
           vertical-align: middle;
           width: 100%;
         }
-        pre {
+        textarea {
           border: 0;
           box-sizing: border-box;
           font: small/normal monospace;
@@ -49,7 +49,7 @@ export class ShaderExpress extends Dummy {
           white-space: pre-wrap;
           width: 100%;
         }
-        pre:focus {
+        textarea:focus {
           outline: 1px dotted;
           outline-offset: 1px;
         }
@@ -70,10 +70,10 @@ export class ShaderExpress extends Dummy {
           background: whitesmoke;
         }
       </style>
-      <video height="${this.height}" width="${this.width}" fps="${this.#fps}" muted playsinline></video>
+      <video height="${this.height}" width="${this.width}" muted playsinline></video>
       <dialog>
         <form method="dialog">
-          <pre id="code" contenteditable spellcheck="false"></pre>
+          <textarea spellcheck="false"></textarea>
           <menu>
             <li><input type="submit" value="Update"></li>
             <li><button value="Cancel">Back</button></li>
@@ -85,22 +85,91 @@ export class ShaderExpress extends Dummy {
 
     this.attachShadow({ mode: "open" })
     this.shadowRoot.appendChild(template.cloneNode(true))
+
+    const textarea = this.shadowRoot.querySelector("textarea")
+    const form = this.shadowRoot.querySelector("form")
+
+    form.addEventListener("reset", () => {
+      const inputevent = new InputEvent("input", { data: this.textContent.trim() })
+
+      textarea.dispatchEvent(inputevent)
+    })
+
+    const video = this.shadowRoot.querySelector("video")
+    const dialog = this.shadowRoot.querySelector("dialog")
+
+    dialog.addEventListener("close", () => {
+      // NOTE: Fires on cancel too.
+      if (dialog.returnValue === "Update") {
+        this.textContent = textarea.value
+      }
+
+      if (document.pictureInPictureElement) {
+        document.exitPictureInPicture()
+      }
+    })
+
+    this.addEventListener("edit", async function onedit() {
+      if (document.pictureInPictureEnabled) {
+        try {
+          await video.requestPictureInPicture()
+        } catch (e) {
+          throw e
+        }
+      }
+
+      dialog.showModal()
+    })
+
+    this.addEventListener("open", function onopen() {
+      const input = document.createElement("input")
+      const reader = new FileReader()
+
+      reader.addEventListener("load", function onload(e) {
+        const inputevent = new InputEvent("input", { data: e.target.result })
+
+        textarea.dispatchEvent(inputevent)
+      })
+
+      input.setAttribute("accept", ".glsl")
+      input.setAttribute("type", "file")
+      input.addEventListener("change", function onchange() {
+        const [f] = input?.files
+
+        reader.readAsText(f)
+      })
+
+      input.click()
+    })
+
+    this.addEventListener("save", function onedit() {
+      const file = new File([textarea.value], `sketch-${Date.now()}.glsl`, { type: "text/plain" })
+      const url = URL.createObjectURL(file)
+      const link = document.createElement("a")
+
+      link.setAttribute("download", file.name)
+      link.setAttribute("href", url)
+      link.click()
+    })
   }
   #currentTime = document.timeline.currentTime
-  #fps = 55
   #lastTime = null
+  #fps = 55
   #frame = null
-  get width() {
-    return this.getAttribute("width")
-  }
-  set width(v) {
-    this.setAttribute("width", v)
+  static get observedAttributes() {
+    return ["height", "width", "src", "fps"]
   }
   get height() {
     return this.getAttribute("height")
   }
   set height(v) {
     this.setAttribute("height", v)
+  }
+  get width() {
+    return this.getAttribute("width")
+  }
+  set width(v) {
+    this.setAttribute("width", v)
   }
   get fps() {
     return this.getAttribute("fps")
@@ -114,20 +183,17 @@ export class ShaderExpress extends Dummy {
   set controls(v) {
     this.setAttribute("controls", v)
   }
-  get poster() {
-    return this.getAttribute("poster")
-  }
-  set poster(v) {
-    this.setAttribute("poster", v)
-  }
   get autoplay() {
     return this.hasAttribute("autoplay")
   }
   set autoplay(v) {
     this.setAttribute("autoplay", v)
   }
-  static get observedAttributes() {
-    return ["height", "width", "src", "fps"]
+  get poster() {
+    return this.getAttribute("poster")
+  }
+  set poster(v) {
+    this.setAttribute("poster", v)
   }
   async attributeChangedCallback(_, oldValue, newValue) {
     if (oldValue !== null && newValue !== null && newValue !== oldValue) {
@@ -135,104 +201,53 @@ export class ShaderExpress extends Dummy {
     }
   }
   async connectedCallback() {
+    // NOTE: Not sure if this makes a difference here.
     if (!this.isConnected) {
       return
     }
 
     try {
-      const editor = this.shadowRoot.querySelector("pre[contenteditable]")
-      const dialog = this.shadowRoot.querySelector("dialog")
-
-      dialog.addEventListener("close", () => {
-        // NOTE: Fires on cancel too.
-        if (dialog.returnValue === "Update") {
-          this.textContent = editor.textContent
-        }
-
-        if (document.pictureInPictureElement) {
-          document.exitPictureInPicture()
-        }
-      })
-
-      const form = this.shadowRoot.querySelector("form")
-
-      form.addEventListener("reset", () => {
-        editor.textContent = this.textContent.trim()
-      })
-
+      const textarea = this.shadowRoot.querySelector("textarea")
       const video = this.shadowRoot.querySelector("video")
-      const { width: w, height: h } = video
+
+      const { width, height } = this
       const state = {}
-      const { gl, createProgram, createTexture } = glx(w, h)
+      const { gl, createProgram, createTexture } = glx(width, height)
+
+      Object.assign(video, { width, height })
 
       const pointer = [0, 0, 0, 0]
       const tracker = ({ offsetX: x, offsetY: y }) => {
         pointer.splice(0, pointer.length, x, y, x - pointer[0], y - pointer[1])
       }
 
-      this.addEventListener("open", function onopen() {
-        const input = document.createElement("input")
-        const reader = new FileReader()
-
-        reader.addEventListener("load", function onload(e) {
-          editor.textContent = e.target.result
-        })
-
-        input.setAttribute("accept", ".glsl")
-        input.setAttribute("type", "file")
-        input.addEventListener("change", function onchange() {
-          const [f] = input?.files
-
-          reader.readAsText(f)
-        })
-
-        input.click()
-      })
-
-      this.addEventListener("save", function onedit() {
-        const file = new File([editor.textContent], `sketch-${Date.now()}.glsl`, { type: "text/plain" })
-        const url = URL.createObjectURL(file)
-        const link = document.createElement("a")
-
-        link.setAttribute("download", file.name)
-        link.setAttribute("href", url)
-        link.click()
-      })
-
-      this.addEventListener("edit", async function onedit() {
-        if (document.pictureInPictureEnabled) {
-          try {
-            await video.requestPictureInPicture()
-          } catch (e) {
-            throw e
-          }
-        }
-
-        dialog.showModal()
-      })
+      // Collect images into textures.
+      const textureloader = bulkloader(createTexture)
+      const textures = await textureloader(
+        ...new Set(Array.from(this.children)
+          .filter(c => c instanceof ShaderExpressTexture)
+          .map(c => c.src))
+      )
 
       // Collect dependencies.
       const moduleloader = bulkloader(textreader)
       const modules = await moduleloader(
-        ...Array.from(this.children)
+        ...new Set(Array.from(this.children)
           .filter(c => c instanceof ShaderExpressModule)
-          .map(c => c.src)
+          .map(c => c.src))
       )
 
-      // Collect images into textures.
-      const textureloader = bulkloader(createTexture)
-      const textures = await textureloader(
-        ...Array.from(this.children)
-          .filter(c => c instanceof ShaderExpressTexture)
-          .map(c => c.src)
-      )
+      // Update content before attaching the input watcher.
+      textarea.oninput = (e) => {
+        if (e.inputType === "" && e.data !== null) {
+          textarea.value = textarea.textContent = e.data
+        }
 
-      const observer = new MutationObserver(function onmutate() {
         const program = createProgram(
           vertexshader(),
           fragmentshader(
-            editor.textContent,
-            ...textures.map((_, id) => `uniform sampler2D uTexture${id};\n`),
+            textarea.value,
+            ...textures.map((t) => `uniform sampler2D u${t.id};`),
             ...modules,
           )
         )
@@ -240,29 +255,24 @@ export class ShaderExpress extends Dummy {
         gl.useProgram(program)
 
         // Map textures into uniform locations.
-        textures.forEach((_, i) => {
-          const id = `uTexture${i}`
+        textures.forEach((t) => {
+          const id = `u${t.id}`
 
           state[id] = gl.getUniformLocation(program, id)
         })
 
         state.uPointer = gl.getUniformLocation(program, "uPointer")
         state.uResolution = gl.getUniformLocation(program, "uResolution")
+        state.uWindow = gl.getUniformLocation(program, "uWindow")
         state.uTime = gl.getUniformLocation(program, "uTime")
         state.aPosition = gl.getAttribLocation(program, "aPosition")
 
         if (state.aPosition !== -1) {
           gl.enableVertexAttribArray(state.aPosition)
         }
-      })
+      }
 
-      // Input event no good.
-      observer.observe(editor, { characterData: true, childList: true, subtree: true })
-
-      // Trigger program compilation.
-      editor.textContent = this.src ? await filereader(this.src) : this.textContent.trim()
-
-      const edges = Float32Array.of(0, 0, 0, h, w, 0, w, 0, 0, h, w, h)
+      const edges = Float32Array.of(0, 0, 0, height, width, 0, width, 0, 0, height, width, height)
       const stride = 2 * Float32Array.BYTES_PER_ELEMENT
       const shape = gl.createBuffer()
 
@@ -270,21 +280,21 @@ export class ShaderExpress extends Dummy {
       gl.bufferData(gl.ARRAY_BUFFER, edges, gl.STATIC_DRAW)
       gl.bindBuffer(gl.ARRAY_BUFFER, null)
 
+      textures.forEach((t, i) => {
+        const id = `u${t.id}`
+
+        if (t.id in state) {
+          gl.activeTexture(gl.TEXTURE0 + i)
+          gl.bindTexture(gl.TEXTURE_2D, t.texture)
+          gl.uniform1i(state[id], i)
+        }
+      })
+
       const loop = (timestamp) => {
         gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
         gl.clearColor(1, 1, 1, 1)
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
         gl.bindBuffer(gl.ARRAY_BUFFER, shape)
-
-        textures.forEach((texture, i) => {
-          const id = `uTexture${i}`
-
-          if (id in state) {
-            gl.activeTexture(gl.TEXTURE0 + i)
-            gl.bindTexture(gl.TEXTURE_2D, texture)
-            gl.uniform1i(state[id], i)
-          }
-        })
 
         if ("uPointer" in state) {
           gl.uniform4f(state.uPointer, ...pointer)
@@ -298,7 +308,11 @@ export class ShaderExpress extends Dummy {
         }
 
         if ("uResolution" in state) {
-          gl.uniform2f(state.uResolution, w, h)
+          gl.uniform2f(state.uResolution, width, height)
+        }
+
+        if ("uWindow" in state) {
+          gl.uniform2f(state.uWindow, self.innerWidth, self.innerHeight)
         }
 
         if ("aPosition" in state) {
@@ -310,14 +324,11 @@ export class ShaderExpress extends Dummy {
       }
 
       video.controls = this.controls
-      video.srcObject = gl.canvas.captureStream(this.#fps)
-      video.poster = this.poster
+      video.srcObject = gl.canvas.captureStream(Number(this.fps))
       video.autoplay = this.autoplay
 
-      video.onplay = () => {
-        document.addEventListener("pointermove", tracker, { passive: true })
-
-        this.#frame = this.#frame ?? requestAnimationFrame(loop)
+      if (this.poster) {
+        video.poster = this.poster
       }
 
       video.onpause = () => {
@@ -326,11 +337,25 @@ export class ShaderExpress extends Dummy {
         this.#frame = this.#lastTime = cancelAnimationFrame(this.#frame)
       }
 
-      if (video.autoplay) {
+      // These being on style handlers means they replace any previous ones.
+      video.onplay = () => {
+        document.addEventListener("pointermove", tracker, { passive: true })
+
+        this.#frame = this.#frame ?? requestAnimationFrame(loop)
+      }
+
+      if (this.autoplay) {
+        this.#frame = this.#lastTime = cancelAnimationFrame(this.#frame)
         this.#frame = requestAnimationFrame(loop)
       }
 
       // All set.
+      const inputevent = new InputEvent("input", {
+        data: this.src ? await textreader(this.src) : this.textContent.trim()
+      })
+
+      textarea.dispatchEvent(inputevent)
+
       const readyevent = new CustomEvent("ready", { detail: { video } })
 
       this.dispatchEvent(readyevent)
@@ -349,10 +374,11 @@ precision highp float;
 in vec2 vUv;
 uniform vec4 uPointer;
 uniform vec2 uResolution;
+uniform vec2 uWindow;
 uniform float uTime;
 out vec4 oColor;
 
-${extras.join("")}
+${extras.join("\n")}
 ${s}
 
 void main() {
@@ -374,6 +400,11 @@ void main() {
 }`
 }
 
+// Helps with string to HTML coversion.
+export function ranger(s = "") {
+  return document.createRange().createContextualFragment(s)
+}
+
 // Helps load promisified things in bulk.
 export function bulkloader(loader) {
   return function load(...list) {
@@ -384,11 +415,6 @@ export function bulkloader(loader) {
 // Helps read in files as text.
 export function textreader(path) {
   return fetch(path).then(r => r.ok && r.text())
-}
-
-// Helps with string to HTML coversion.
-export function ranger(s = "") {
-  return document.createRange().createContextualFragment(s)
 }
 
 // Provides helpers for program and texture creation tied
@@ -404,35 +430,33 @@ export function glx(width, height) {
 
 // Helps load images into textures.
 export function texturecreator(gl) {
-  return function createTexture(src, crossorigin, texture = gl.createTexture()) {
+  return function createTexture(src, crossorigin) {
+    const texture = gl.createTexture()
+
     gl.bindTexture(gl.TEXTURE_2D, texture)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
 
     return new Promise((resolve, reject) => {
-      if (src) {
-        const image = new Image()
+      const image = new Image()
 
-        image.onerror = (e) => {
-          reject(e)
-        }
+      image.onerror = (e) => {
+        reject(e)
+      }
 
-        image.onload = () => {
-          const { width: w, height: h } = image
+      image.onload = () => {
+        const { width: w, height: h } = image
 
-          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, image)
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, image)
 
-          resolve(texture)
-        }
+        resolve({ texture, id: src.split(".").shift().toUpperCase() })
+      }
 
-        image.src = src
+      image.src = src
 
-        if (crossorigin !== undefined) {
-          image.crossOrigin = crossorigin
-        }
-      } else {
-        resolve(texture)
+      if (crossorigin !== undefined) {
+        image.crossOrigin = crossorigin
       }
     })
   }
